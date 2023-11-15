6be9b8eaf93b96b898ebd9bff7dd005b6c95f47c
Adrian Moore <adrianrob@hotmail.com>
2023-11-15 03:30:53 -0800

    Fixed problem with route scalable and resixable not being disabled

--- projects/three-flow/src/lib/route.ts
+++ projects/three-flow/src/lib/route.ts
@@ -8,13 +8,13 @@ export class FlowRoute extends FlowNode {
 
   constructor(graph: FlowGraph, private route: FlowRouteParameters) {
     route.type = 'route'
-    route.resizable = route.scalable = false // don't allow
     route.radius = route.radius ? route.radius : 0.1
     route.height = route.width = route.radius
 
     super(graph, route);
 
     this.radius = route.radius!
+    this.resizable = this.scalable = false // don't allow
   }
 
   override createGeometry(): BufferGeometry {
